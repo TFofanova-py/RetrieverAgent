3630b0f4811098c3e1843efa624a175dc791d0de
Tatiana Fofanova <tatiana.fofanova@gmail.com>
2024-10-28 18:01:19 +0100

    simple retriever

--- agent.py
+++ agent.py
@@ -55,51 +55,41 @@ async def setup_agent(model: str, verbose: bool = False) -> Tuple[CompiledStateG
         state["docs"] = []
         return state
 
-    async def cls_query(state: State) -> Literal["call_model", "retrieve"]:
-        is_query_response = await llm.ainvoke(
-            f"I give you a text. Your task is to decide if the text is a query for information. Output must be just yes or no.\nText: {state["messages"][-1].content}")
-        is_query = is_query_response.content.lower().strip()
-        logger.info(f"Classificator: is a query, {is_query}")
-
-        if any([is_query.startswith(x) for x in ["yes", "ja"]]):
-            return "retrieve"
-        else:
-            return "call_model"
-
-    async def retrieve(state: State) -> State:
-        state["docs"] += await history_aware_retriever.ainvoke({"input": state["messages"][-1].content, "chat_history": state["messages"][-6:]})
-        logger.info(f"Retrieved documents: {state["docs"]}")
-        return state
-
-    async def generate(state: State) -> State:
-
-        ret_task = asyncio.create_task(rag_chain.ainvoke({"context": state["docs"], "question": state["messages"][-1]}))
+    async def create_answer(state: State) -> State:
+        is_query_task = asyncio.create_task(llm.ainvoke(f"I give you a text. Your task is to decide if the text is a query for information. Output must be just yes or no.\nText: {state["messages"][-1].content}"))
+        ret_task = asyncio.create_task(retriever.ainvoke(state["messages"][-1].content))
+        # ret_task = asyncio.create_task(history_aware_retriever.ainvoke({"input": state["messages"][-1].content, "chat_history": state["messages"][-6:]}))
         llm_task = asyncio.create_task(llm.ainvoke([SystemMessage(content="Always return output in German."),
-                                        state["messages"][-1]]))
-        ret_answer = await ret_task
+                                                    state["messages"][-1]]))
 
-        grade = await llm.ainvoke(
-            f"I give you a text of answer. Your task is to decide if the text means \"I don't know\". Output must be just yes or no.\nText: {ret_answer}")
-        grade_content = grade.content.lower().strip()
-        logger.info(f"Grading answer, {grade_content}")
+        is_query = (await is_query_task).content.lower().strip()
 
-        if any([grade_content.startswith(x) for x in ["no", "nein"]]):
-            llm_task.cancel()
-            state["answer"] = ret_answer
-        else:
+        if any([is_query.startswith(x) for x in ["no", "nein"]]):
+            logger.info("Classificator: it's not a query. LLM answers")
+            ret_task.cancel()
             state["answer"] = (await llm_task).content
+            logger.info(f"LLM: {state["answer"]}")
+        else:
+            logger.info("Classificator: it's a query. Retriever answers")
+            state["docs"] = await ret_task
+            logger.info(f"Retriever documents: {state["docs"]}")
+
+            ret_answer = await asyncio.create_task(
+                rag_chain.ainvoke({"context": state["docs"], "question": state["messages"][-1]}))
+            logger.info(f"Retrieved: {ret_answer}")
+
+            grade = await llm.ainvoke(
+                f"I give you a text of answer. Your task is to decide if the text means \"I don't know\". Output must be just yes or no.\nText: {ret_answer}")
+            grade_content = grade.content.lower().strip()
+            logger.info(f"Grading answer, {grade_content}")
+
+            if any([grade_content.startswith(x) for x in ["no", "nein"]]):
+                llm_task.cancel()
+                state["answer"] = ret_answer
+            else:
+                state["answer"] = (await llm_task).content
         return state
 
-
-    async def call_model(state: State) -> State:
-        msg = ""
-        async for chunk in llm.astream([SystemMessage(content="Always return output in German."),
-                                        state["messages"][-1]]):
-            print(chunk.content, end="")
-            msg += chunk.content
-        state["messages"].append(AIMessage(content=msg))
-        logger.info(f"Calling model, {state["messages"][-1]}")
-        return state
 
     async def save_information(state: State) -> State:
         logger.info("Save information")
@@ -149,7 +139,7 @@ async def setup_agent(model: str, verbose: bool = False) -> Tuple[CompiledStateG
         **db_kwargs
     )
 
-    llm = ChatOllama(model=model, temperature=0)
+    llm = ChatOllama(model=model, temperature=0.)
     retriever = db.as_retriever()
 
     history_aware_retriever = create_history_aware_retriever(
@@ -161,18 +151,13 @@ async def setup_agent(model: str, verbose: bool = False) -> Tuple[CompiledStateG
 
     workflow = StateGraph(State)
     workflow.add_node("set_initial_state", set_initial_state)
-    workflow.add_node("retrieve", retrieve)
-    workflow.add_node("generate", generate)
-    # workflow.add_node("choose_answer", choose_answer)
-    workflow.add_node("call_model", call_model)
+    workflow.add_node("create_answer", create_answer)
     workflow.add_node("save_information", save_information)
     workflow.add_node("save_answer", save_answer)
 
     workflow.add_conditional_edges(START, has_save_tag)
-    workflow.add_conditional_edges("set_initial_state", cls_query)
-    workflow.add_edge("retrieve", "generate")
-    workflow.add_edge("generate", "save_answer")
-    workflow.add_edge("call_model", END)
+    workflow.add_edge("set_initial_state", "create_answer")
+    workflow.add_edge("create_answer", "save_answer")
     workflow.add_edge("save_information", END)
     workflow.add_edge("save_answer", END)
 
