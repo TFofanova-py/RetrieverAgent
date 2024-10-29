47f42c6564f5635021c23926e6ea0b2d7d0f8508
Tatiana Fofanova <tatiana.fofanova@gmail.com>
2024-10-29 13:01:08 +0100

    add streaming

--- agent.py
+++ agent.py
@@ -1,28 +1,22 @@
 import asyncio
+
+from httpcore import stream
 from langchain_ollama import ChatOllama, OllamaEmbeddings
 from langchain_community.vectorstores import OpenSearchVectorSearch
 from langchain import hub
 from langchain_core.output_parsers import StrOutputParser
-# from langchain_community.tools.tavily_search import TavilySearchResults
 import json
-from langgraph.checkpoint.memory import MemorySaver
-from langgraph.graph import StateGraph, START, END
-from langgraph.graph import MessagesState
-from langgraph.graph.state import CompiledStateGraph
 from langchain.chains import create_history_aware_retriever
-from typing import List, Literal, Tuple
+from typing import List
 from langchain_core.documents import Document
 import logging
 from datetime import datetime
 from langchain_core.prompts import MessagesPlaceholder
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.messages import SystemMessage, AIMessage, BaseMessage
+from langchain_core.messages import SystemMessage, AIMessage, BaseMessage, HumanMessage
 import argparse
 from make_index.setup_opensearch_db import process_chunk
 
-
-# _set_env("TAVILY_API_KEY")
-# web_search_tool = TavilySearchResults(k=3)
 
 logging.basicConfig(
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -32,169 +26,142 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-class State(MessagesState):
-    docs: List[Document]
-    answer: str
-    verbose: bool
+class Agent:
+    def __init__(self, model: str, verbose: bool = False):
+        self.docs: List[Document] = []
+        self.model = model
+        self.verbose = verbose
+        self.answer: str = ""
+        self.messages: List[BaseMessage] = []
+        self.embedder = OllamaEmbeddings(model="llama3")
+        self.llm = ChatOllama(model=model, temperature=0.)
+        self.db_kwargs = json.load(open("creds.json", "rb"))["OPEN_SEARCH_KWARGS"]
+        self.db_kwargs["http_auth"] = (
+            self.db_kwargs.get("http_auth", {}).get("login"),
+            self.db_kwargs.get("http_auth", {}).get("password")
+        )
+        self.db = OpenSearchVectorSearch(
+            embedding_function=self.embedder,
+            **self.db_kwargs
+        )
+        self.index_name = self.db_kwargs.get("index_name")
+        self.retriever = self.db.as_retriever()
+        contextualize_q_system_prompt = (
+            "Given a chat history and the latest user question "
+            "which might reference context in the chat history, "
+            "formulate a standalone question which can be understood "
+            "without the chat history. Do NOT answer the question, "
+            "just reformulate it if needed and otherwise return it as is."
+        )
+        contextualize_q_prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", contextualize_q_system_prompt),
+                MessagesPlaceholder("chat_history"),
+                ("human", "{input}"),
+            ]
+        )
+        self.history_aware_retriever = create_history_aware_retriever(
+            self.llm, self.retriever, contextualize_q_prompt
+        )
+        rag_prompt = hub.pull("rlm/rag-prompt")
+        self.rag_chain = rag_prompt | self.add_lang_prompt | self.llm | StrOutputParser()
+        logger.info("Init agent")
 
+    @staticmethod
+    def add_lang_prompt(inputs):
+        inputs.messages = [SystemMessage("Always return output in German.")] + inputs.messages
+        return inputs
 
-def add_lang_prompt(inputs):
-    inputs.messages = [SystemMessage("Always return output in German.")] + inputs.messages
-    return inputs
+    async def has_save_tag(self) -> bool:
+        return self.messages[-1].content.strip().lower().startswith("#save")
 
-
-async def setup_agent(model: str, verbose: bool = False) -> Tuple[CompiledStateGraph, dict]:
-    async def has_save_tag(state: State) -> Literal["save_information", "set_initial_state"]:
-        if state["messages"][-1].content.strip().lower().startswith("#save"):
-            return "save_information"
-        return "set_initial_state"
-
-    def set_initial_state(state: State):
-        logger.info("Set initial state")
-        state["verbose"] = verbose
-        state["docs"] = []
-        return state
-
-    async def create_answer(state: State) -> State:
-        is_query_task = asyncio.create_task(llm.ainvoke(f"I give you a text. Your task is to decide if the text is a query for information. Output must be just yes or no.\nText: {state["messages"][-1].content}"))
-        ret_task = asyncio.create_task(retriever.ainvoke(state["messages"][-1].content))
-        # ret_task = asyncio.create_task(history_aware_retriever.ainvoke({"input": state["messages"][-1].content, "chat_history": state["messages"][-6:]}))
-        llm_task = asyncio.create_task(llm.ainvoke([SystemMessage(content="Always return output in German."),
-                                                    state["messages"][-1]]))
+    async def create_answer(self):
+        is_query_task = asyncio.create_task(self.llm.ainvoke(f"I give you a text. Your task is to decide if the text is a query for information. Output must be just yes or no.\nText: {self.messages[-1].content}"))
+        # ret_task = asyncio.create_task(self.retriever.ainvoke(self.messages[-1].content))
+        # ret_task = asyncio.create_task(self.history_aware_retriever.ainvoke({"input": state["messages"][-1].content, "chat_history": state["messages"][-6:]}))
+        # llm_task = asyncio.create_task(self.llm.astream([SystemMessage(content="Always return output in German."), self.messages[-1]]))
 
         is_query = (await is_query_task).content.lower().strip()
 
         if any([is_query.startswith(x) for x in ["no", "nein"]]):
             logger.info("Classificator: it's not a query. LLM answers")
-            ret_task.cancel()
-            state["answer"] = (await llm_task).content
-            logger.info(f"LLM: {state["answer"]}")
+            # ret_task.cancel()
+            async for chunk in self.llm.astream([SystemMessage(content="Always return output in German."), self.messages[-1]]):
+                yield chunk
         else:
             logger.info("Classificator: it's a query. Retriever answers")
-            state["docs"] = await ret_task
-            logger.info(f"Retriever documents: {state["docs"]}")
+            self.docs = await self.retriever.ainvoke(self.messages[-1].content)  # ret_task
+            logger.info(f"Retriever documents: {self.docs}")
 
             ret_answer = await asyncio.create_task(
-                rag_chain.ainvoke({"context": state["docs"], "question": state["messages"][-1]}))
+                self.rag_chain.ainvoke({"context": self.docs, "question": self.messages[-1]}))
             logger.info(f"Retrieved: {ret_answer}")
 
-            grade = await llm.ainvoke(
+            grade = await self.llm.ainvoke(
                 f"I give you a text of answer. Your task is to decide if the text means \"I don't know\". Output must be just yes or no.\nText: {ret_answer}")
             grade_content = grade.content.lower().strip()
             logger.info(f"Grading answer, {grade_content}")
 
             if any([grade_content.startswith(x) for x in ["no", "nein"]]):
-                llm_task.cancel()
-                state["answer"] = ret_answer
+                # llm_task.cancel()
+                yield ret_answer
             else:
-                state["answer"] = (await llm_task).content
-        return state
+                async for chunk in self.llm.astream([SystemMessage(content="Always return output in German."), self.messages[-1]]):
+                    yield chunk
+
+    async def save_answer(self):
+        logger.info("Save answer")
+        self.messages.append(AIMessage(self.answer))
 
 
-    async def save_information(state: State) -> State:
+    async def save_information(self):
         logger.info("Save information")
-        text, embs, metadata = await process_chunk(state["messages"][-1].content.replace("#save", ""),
+        text, embs, metadata = await process_chunk(self.messages[-1].content.replace("#save", ""),
                                                    "chat_message",
                                                    datetime.now())
         text_emb_data = [(text, emb) for emb in embs if emb]
         logger.info("Adding embeddings")
-        db.add_embeddings(text_emb_data,
+        self.db.add_embeddings(text_emb_data,
                           metadatas=[metadata] * len(embs),
-                          index_name=index_name
+                          index_name=self.index_name
                           )
-        state["messages"].append(AIMessage(f"Informationen werden im Index {index_name} gespeichert"))
-        print(state["messages"][-1].content)
-        return state
+        self.messages.append(AIMessage(f"Informationen werden im Index {self.index_name} gespeichert"))
 
-    async def save_answer(state: State) -> State:
-        logger.info("Save answer")
-        state["messages"].append(AIMessage(state["answer"]))
-        print(state["messages"][-1].content)
-        return state
 
-    if verbose:
-        logger.addHandler(logging.StreamHandler())
-
-    contextualize_q_system_prompt = (
-        "Given a chat history and the latest user question "
-        "which might reference context in the chat history, "
-        "formulate a standalone question which can be understood "
-        "without the chat history. Do NOT answer the question, "
-        "just reformulate it if needed and otherwise return it as is."
-    )
-    contextualize_q_prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system", contextualize_q_system_prompt),
-            MessagesPlaceholder("chat_history"),
-            ("human", "{input}"),
-        ]
-    )
-
-    embedder = OllamaEmbeddings(model="llama3")
-    db_kwargs = json.load(open("creds.json", "rb"))["OPEN_SEARCH_KWARGS"]
-    db_kwargs["http_auth"] = (db_kwargs.get("http_auth", {}).get("login"), db_kwargs.get("http_auth", {}).get("password"))
-    index_name = db_kwargs.get("index_name")
-    db = OpenSearchVectorSearch(
-        embedding_function=embedder,
-        **db_kwargs
-    )
-
-    llm = ChatOllama(model=model, temperature=0.)
-    retriever = db.as_retriever()
-
-    history_aware_retriever = create_history_aware_retriever(
-        llm, retriever, contextualize_q_prompt
-    )
-
-    rag_prompt = hub.pull("rlm/rag-prompt")
-    rag_chain = rag_prompt | add_lang_prompt | llm | StrOutputParser()
-
-    workflow = StateGraph(State)
-    workflow.add_node("set_initial_state", set_initial_state)
-    workflow.add_node("create_answer", create_answer)
-    workflow.add_node("save_information", save_information)
-    workflow.add_node("save_answer", save_answer)
-
-    workflow.add_conditional_edges(START, has_save_tag)
-    workflow.add_edge("set_initial_state", "create_answer")
-    workflow.add_edge("create_answer", "save_answer")
-    workflow.add_edge("save_information", END)
-    workflow.add_edge("save_answer", END)
-
-    memory = MemorySaver()
-    graph = workflow.compile(checkpointer=memory)
-    config = {"configurable": {"thread_id": "1"}}
-    return graph, config
 
 async def main_gr():
     import gradio as gr
 
+
     async def chat_response(input_message, chat_history):
         start = datetime.now()
         logger.info(f"Input message: {input_message}")
-        output = await agent.ainvoke({"messages": [input_message]}, config)
-        chat_history.append((input_message, output["messages"][-1].content))
+        agent.messages.append(HumanMessage(input_message))
+        msg = ""
+        async for chunk in agent.create_answer():
+            msg += chunk if isinstance(chunk, str) else chunk.content
+            yield msg
+
+        agent.messages.append(AIMessage(msg))
         logger.info(f"Response time: {datetime.now() - start}")
 
-        return "", chat_history
 
     # parser = argparse.ArgumentParser()
     # parser.add_argument("-m", "--model", default="llama3", help="Model to use")
     #
     # args = parser.parse_args()
 
-    agent, config =  await setup_agent(model="llama3.1", verbose=False)
+    agent =  Agent(model="llama3.1", verbose=False)
     logger.info("Agent started")
 
     with gr.Blocks() as demo:
-        chatbot = gr.Chatbot(type="tuples")
-        message = gr.Textbox(placeholder="Type a message...", label="Your Message")
-        submit = gr.Button("Send")
-
-        # Bind the async function to the submit button
-        submit.click(chat_response, inputs=[message, chatbot], outputs=[message, chatbot])
-
+        chatbot = gr.ChatInterface(fn=chat_response,
+                                   type="messages",
+                                   chatbot=gr.Chatbot(type="messages", value=[{"role": "assistant", "content": "Hallo! Ich bin Ihr ISMS-Assistent. Stellen Sie Fragen"}]),
+                                   textbox=gr.Textbox(placeholder="Type a message...", label="Your Message")
+                                   )
     demo.launch(share=True)
+
 
 if __name__ == "__main__":
     asyncio.run(main_gr())
